efe82d9353705630a308ca5bb67e0d70887a81ca
yzn <66845701@qq.com>
2018-08-14 23:06:13 +0800

    ss

---
+++ shadowsocksr-csharp-4.9.0/shadowsocks-csharp/View/FreeSSR.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Shadowsocks.View
+{
+    partial class FreeSSRForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRefreshPage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.chkDeleteOld = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnRefreshPage
+            // 
+            this.btnRefreshPage.Location = new System.Drawing.Point(317, 38);
+            this.btnRefreshPage.Name = "btnRefreshPage";
+            this.btnRefreshPage.Size = new System.Drawing.Size(97, 23);
+            this.btnRefreshPage.TabIndex = 15;
+            this.btnRefreshPage.Text = "Refresh Page";
+            this.btnRefreshPage.UseVisualStyleBackColor = true;
+            this.btnRefreshPage.Click += new System.EventHandler(this.btnRefreshPage_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(473, 12);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "使用说明: 请等待页面加载完后, 复制表格中的一个IP到文本框中, 再按\"开始执行\"按钮";
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(236, 38);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 13;
+            this.btnExecute.Text = "开始执行";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 21);
+            this.textBox1.TabIndex = 12;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 68);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(3, 50, 3, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(691, 312);
+            this.webBrowser1.TabIndex = 11;
+            this.webBrowser1.Url = new System.Uri("https://free-ss.site/", System.UriKind.Absolute);
+            // 
+            // chkDeleteOld
+            // 
+            this.chkDeleteOld.AutoSize = true;
+            this.chkDeleteOld.Location = new System.Drawing.Point(420, 42);
+            this.chkDeleteOld.Name = "chkDeleteOld";
+            this.chkDeleteOld.Size = new System.Drawing.Size(132, 16);
+            this.chkDeleteOld.TabIndex = 16;
+            this.chkDeleteOld.Text = "删除原有的配置文件";
+            this.chkDeleteOld.UseVisualStyleBackColor = true;
+            
+            // 
+            // FreeSSR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 392);
+            this.Controls.Add(this.chkDeleteOld);
+            this.Controls.Add(this.btnRefreshPage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "FreeSSR";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FreeSSR";
+            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRefreshPage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.CheckBox chkDeleteOld;
+    }
+}
---
+++ shadowsocksr-csharp-4.9.0/shadowsocks-csharp/View/FreeSSR.cs
@@ -0,0 +1,143 @@
+﻿using Newtonsoft.Json;
+using Shadowsocks.Controller;
+using Shadowsocks.FreeSSR;
+using Shadowsocks.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+namespace Shadowsocks.View
+{
+    public partial class FreeSSRForm : Form
+    {
+        private const string SSR_EXE_PATH = @"E:\shadowsocksr-csharp-4.9.0\shadowsocks-csharp\bin\4.0\Debug";
+        private ShadowsocksController controller;
+        public FreeSSRForm(ShadowsocksController controller)
+        {
+           
+            InitializeComponent();
+            this.controller = controller;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://free-ss.site/");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text.Trim()))
+            {
+                MessageBox.Show("请先在文本中输入一个IP!");
+                return;
+            }
+            var s = webBrowser1.Document.Body.InnerHtml; ;
+            var reg = new System.Text.RegularExpressions.Regex("<tbody>(.*)<\\/tbody>");
+            foreach (Match m in reg.Matches(s))
+            {
+                var xx = m.ToString();
+                if (xx.Contains(textBox1.Text))
+                {
+                    Console.WriteLine(m);
+                    var sbOut = new System.Text.StringBuilder();
+                    sbOut.AppendLine(@"<html>");
+                    sbOut.AppendLine("<table>" + m.ToString() + "</table>");
+                    sbOut.AppendLine("</html");
+                    var sFileTemplate = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "1.html");
+                    var sOut = System.IO.File.ReadAllText(sFileTemplate);
+                    sOut = sOut.Replace("${SSTABLE}", "<table>" + m.ToString() + "</table>");
+                    var sFileOut = getFilePath("2.html");
+                    System.IO.File.WriteAllText(sFileOut, sOut);
+                    ToSSRJson();
+
+                    var _config= Configuration.Load();
+                    Configuration.Save(_config);
+                    controller.GetConfiguration();
+                    
+
+
+
+                }
+            }
+            Console.WriteLine(s);
+        }
+        private string getFilePath(string file)
+        {
+            return System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file);
+        }
+        private void ToSSRJson()
+        {
+            var sFile = getFilePath("2.html");
+            CsQuery.CQ dom = System.IO.File.ReadAllText(sFile);
+            var oSSR = new SSR();
+            oSSR.configs = new List<FreeSSR.Server>();
+            foreach (var row in dom["tr"])
+            {
+                CsQuery.CQ rowDom = row.InnerHTML;
+                var tds = rowDom["td"].ToList();
+                var config = new FreeSSR.Server();
+                if (tds.Count > 0)
+                {
+                    var vtm = tds[0].InnerText;
+                    if (int.Parse(vtm.Split('/')[1]) > 8) //T是电信线路,值越大越好
+                    {
+                        config.server = tds[1].InnerText;
+                        config.server_port = int.Parse(tds[2].InnerText);
+                        var p1 = tds[3].InnerText;
+                        var p2 = tds[4].InnerText;
+                        if (p1 == "rc4-md5" || p1 == "chacha20" || p1.StartsWith("aes-"))
+                        {
+                            config.method = p1;
+                            config.password = p2;
+                        }
+                        else
+                        {
+                            config.method = p2;
+                            config.password = p1;
+                        }
+                        config.id = Guid.NewGuid().ToString("N");
+                    }
+                }
+                if (!string.IsNullOrWhiteSpace(config.server))
+                    oSSR.configs.Add(config);
+            }
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(oSSR);
+            System.IO.File.WriteAllText(getFilePath("ssr.json"), json);
+            var sOutFile = $"{SSR_EXE_PATH}\\ssr.json";
+            if (System.IO.File.Exists(sOutFile))
+            {
+                System.IO.File.Delete(sOutFile);
+            }
+            System.IO.File.Copy(getFilePath("ssr.json"), sOutFile);
+            write_gui_config(sOutFile);
+        }
+        private void btnRefreshPage_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+        /// <summary>
+        /// 写到gui-config.json文件中
+        /// </summary>
+        private void write_gui_config(string srcFile)
+        {
+            var json = File.ReadAllText(srcFile);
+            var sGuiFile = $"{SSR_EXE_PATH}\\gui-config.json";
+            var srcConfig = JsonConvert.DeserializeObject<SSR>(json);
+            var guiCofnig = JsonConvert.DeserializeObject<SSR>(File.ReadAllText(sGuiFile));
+            if (chkDeleteOld.Checked)
+                guiCofnig.configs.RemoveAll(x => x.group == "FreeSSR-public");
+            guiCofnig.configs.RemoveAll(x =>
+                                       srcConfig.configs.Select(y => y.server)
+                                       .Contains(x.server));
+            guiCofnig.configs.AddRange(srcConfig.configs);
+            File.WriteAllText(sGuiFile, JsonConvert.SerializeObject(guiCofnig));
+        }
+    }
+}
